14bd5e070311262f5ad47c44f33ff2271a22c5a6
Abel <dn10003@gmail.com>
2023-10-09 23:32:22 +0900

    #4_ Vercel 배포 셋팅

--- src/app/components/AchievementsSection.tsx
+++ src/app/components/AchievementsSection.tsx
@@ -40,7 +40,6 @@ const AchievementsSection = () => {
               className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
             >
               <h2 className="text-white text-4xl font-bold flex flex-row">
-                {/* {achievement.prefix} */}
                 <AnimatedNumbers
                   includeComma
                   animateToNumber={parseInt(achievement.value)}
